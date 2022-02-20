d5051d3e21790244c3cadf157c061ee1c017a37f
Artur Oganesyan <artur1948.rf@mail.ru>
2022-02-20 19:26:46 +0300

    fix Task_2

--- hw_2/src/main/kotlin/ru/netology/Task_2.kt
+++ hw_2/src/main/kotlin/ru/netology/Task_2.kt
@@ -2,9 +2,9 @@ package ru.netology
 
 fun main() {
 
-    val likes = 61
+    val likes = 911
 
-    val people = if (likes.toString().endsWith("1")) "человеку" else "людям"
+    val people = if (!likes.toString().endsWith("11") && likes.toString().endsWith("1")) "человеку" else "людям"
 
     println("Понравилось $likes $people")
 }
